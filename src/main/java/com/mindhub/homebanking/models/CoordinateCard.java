d51daf83e51939f844e717c2da7e42c805b1ea29
Benjasjas <benjaminbustosmatus@gmail.com>
2023-05-24 16:23:08 -0400

    se agrega la notacion ElementCollection en el atributo coordinates para mapear las coordenadas en la base de datos

--- src/main/java/com/mindhub/homebanking/models/CoordinateCard.java
+++ src/main/java/com/mindhub/homebanking/models/CoordinateCard.java
@@ -19,35 +19,16 @@ public class CoordinateCard {
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Long id;
+
     @OneToOne
     @JoinColumn(name = "client_id")
     private Client client;
+
+    @ElementCollection
     private Map<String, Integer> coordinates;
 
     public CoordinateCard(Client client) {
         this.client = client;
         this.coordinates = CoordinateCardUtils.generateCoordinates();
     }
-
-     /*public void printCoordinateCard() {
-        System.out.println("Coordinate Card:");
-
-        // Imprimir encabezado de columnas
-        System.out.print("   ");
-        for (char column = 'A'; column <= 'C'; column++) {
-            System.out.print(column + " ");
-        }
-        System.out.println();
-
-        // Imprimir filas y valores
-        for (int row = 1; row <= 3; row++) {
-            System.out.print(row + "  ");
-            for (char column = 'A'; column <= 'C'; column++) {
-                String coordinate = String.valueOf(column) + row;
-                int value = coordinates.getOrDefault(coordinate, 0);
-                System.out.print(value + " ");
-            }
-            System.out.println();
-        }
-    }*/
 }
