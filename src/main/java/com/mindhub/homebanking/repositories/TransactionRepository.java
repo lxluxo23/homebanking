5b327410d56b47e02bcd44c8cccaa62dc89dc8e4
Luis cespedes <lxluxo23@gmail.com>
2023-05-25 15:23:41 -0400

    prototipo buscar por fecha

--- src/main/java/com/mindhub/homebanking/repositories/TransactionRepository.java
+++ src/main/java/com/mindhub/homebanking/repositories/TransactionRepository.java
@@ -2,8 +2,14 @@ package com.mindhub.homebanking.repositories;
 
 import com.mindhub.homebanking.models.Transaction;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
 import org.springframework.data.rest.core.annotation.RepositoryRestResource;
+
+import java.time.LocalDateTime;
+import java.util.List;
 
 @RepositoryRestResource
 public interface TransactionRepository extends JpaRepository<Transaction,Long> {
+    @Query("SELECT t FROM Transaction t WHERE t.date BETWEEN ?1 AND ?2")
+    List<Transaction> findByDateBetween(LocalDateTime startDate, LocalDateTime endDate);
 }
